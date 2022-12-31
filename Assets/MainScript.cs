c758db246928802d2b7de22eb6a1267a77a56c0b
Dodilei <mauricioandrade2002@gmail.com>
2022-12-31 14:46:45 -0400

    Create functional shader workflow

---
+++ Assets/MainScript.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+
+public class MainScript : MonoBehaviour
+{
+    // Set public variables
+
+    public string compute_id = "MainComputeShader";
+    public string shader_id = "Meta/MainShader";
+
+    public string cs_kernel_name = "TestKernel";
+
+    const int vertexCount = 9;
+    const int vertexStride = 2*(4*sizeof(float)) + sizeof(float);
+    
+
+    // Initialize variables and objects
+
+    // Material will contain the main shader pass
+    Material _material;
+
+    // Compute Shader will generate vertices
+    ComputeShader _computeShader;
+    int CSKernel;
+
+    // Buffer to store vertices
+    ComputeBuffer vertexBuffer;
+
+	//Create struct ShaderIDs (shader property ids)
+	//Property IDs are used to get properties faster than name-strings
+	//But how are the properties with same name in all shaders linked?
+	static class ShaderIDs
+	{
+		public static int vertices = Shader.PropertyToID( "_Vertices" );
+	}
+
+    // Awake is always called before Start
+    void Awake()
+    {
+        // Create new material containing main shader pass
+		_material = new Material( Shader.Find( shader_id ) );
+
+        // Load Compute Shader
+		_computeShader = Instantiate( Resources.Load<ComputeShader>( compute_id ) );
+		CSKernel = _computeShader.FindKernel( cs_kernel_name );
+
+        // Create vertex buffer
+		vertexBuffer = new ComputeBuffer( vertexCount, vertexStride );
+
+        // Link vertex buffer to CS main kernel
+		_computeShader.SetBuffer( CSKernel, ShaderIDs.vertices, vertexBuffer );
+
+		// Link vertex buffer to main shader in material
+		_material.SetBuffer( ShaderIDs.vertices, vertexBuffer );
+
+		// Start CS (dim [vertexCount, 1, 1]) and fill vertex buffer
+		_computeShader.Dispatch( CSKernel, vertexCount, 1, 1 );
+    }
+
+    void OnDestroy()
+	{
+		vertexBuffer.Release();
+		Destroy( _computeShader );
+		Destroy( _material );
+	}
+
+	void OnRenderObject()
+	{
+		// Use first defined pass on main shaders
+		_material.SetPass( 0 );
+
+        // Dispatch the main shaders
+		Graphics.DrawProceduralNow( MeshTopology.LineStrip, vertexBuffer.count);
+        // instance count will mirror vertexBuffer size
+        // mesh topology influences how vertex info is sent to VS,
+        //:therefore GS input topology must match it
+	}
+}
---
+++ Assets/Resources/MainShader.shader
@@ -0,0 +1,104 @@
+﻿
+Shader "Meta/MainShader"
+{
+	CGINCLUDE
+			
+	#include "UnityCG.cginc"
+	
+	// struct from CS
+	struct c2v
+	{
+		float4 vertex : SV_POSITION;
+		float width : TEXCOORD0;
+		float4 color : TEXCOORD1;
+	};
+
+	// struct used between VS and GS
+	struct v2g
+	{
+		float4 vertex : SV_POSITION;
+		float width : TEXCOORD0;
+		float4 color : TEXCOORD1;
+	};
+	
+	// struct used between GS and FS
+	struct g2f
+	{
+		float4 vertex : SV_POSITION;
+		float4 color : TEXCOORD1;
+	};
+
+	// buffer which will be read inside VS
+	StructuredBuffer<c2v> _Vertices;
+	
+	// Vertex Shader main function
+	v2g Vert( uint vi : SV_VertexID )
+	{
+		v2g o;
+
+		o.vertex = UnityObjectToClipPos( _Vertices[ vi ].vertex );
+		o.width = _Vertices[ vi ].width;
+		o.color = _Vertices[ vi ].color;
+
+		return o;
+	}
+	
+	// Geometry Shader main function
+	[MaxVertexCount(4)]
+	void Geom( line v2g input[2], inout TriangleStream<g2f> outstream)
+	{
+		// calculate normal
+
+		float2 vec = input[1].vertex.xy - input[0].vertex.xy;
+		float rdst = rsqrt(vec.x*vec.x + vec.y*vec.y);
+
+		float4 normal = float4(0,0,0,0);
+		normal.x = -vec.y * rdst;
+		normal.y = vec.x * rdst;
+
+		// initialize output
+
+		g2f o;
+		o.color = input[0].color;
+
+		// calculate four vertices of a quad to draw a
+		//:linear-variable width line
+
+		o.vertex = input[0].vertex - input[0].width*normal;
+		outstream.Append(o);
+		o.vertex = input[0].vertex + input[0].width*normal;
+		outstream.Append(o);
+
+		o.vertex = input[1].vertex - input[1].width*normal;
+		outstream.Append(o);
+		o.vertex = input[1].vertex + input[1].width*normal;
+		outstream.Append(o);
+
+		// vertex order is important to represent TriStrips
+
+		outstream.RestartStrip();
+	}
+
+	// Fragment Shader main function
+	fixed4 Frag( g2f i ) : SV_Target
+	{
+		return fixed4(i.color);
+	}
+	
+	ENDCG
+	
+
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex Vert
+			#pragma geometry Geom
+			#pragma fragment Frag
+			ENDCG
+		}
+	}
+}
