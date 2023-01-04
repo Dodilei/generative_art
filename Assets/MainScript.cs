2d5bd1f580181c7930a1024c8ff7d9ec1c8b9380
Dodilei <mauricioandrade2002@gmail.com>
2023-01-04 19:31:46 -0400

    Improve multiple render phases

--- Assets/MainScript.cs
+++ Assets/MainScript.cs
@@ -3,13 +3,23 @@ using static Draw;
 
 public class MainScript : DrawBlob
 {
+    public override void Awake()
+    {
+        this.minRadius = 0.05f;
+    }
+
+    public void RunComplete()
+    {
+        base.Awake();
+        base.OnRenderObject();
+        base.OnDestroy();
+    }
+
     public override void OnRenderObject()
     {
-        base.OnRenderObject();
+        this.RunComplete();
         this.minRadius = this.minRadius + 0.1f;
-        this.OnDestroy();
-        this.Awake();
-        base.OnRenderObject(); 
+        this.RunComplete();
         this.enabled = false;
     }
 }
